1cf25cfddc5535e224cebe2c169f1face42cecd2
ShojiMakotoX <250039@jc-21.jp>
2026-07-10 12:16:17 +0900

    Block呼び出し（サイズは調整する必要がありそうだが）

--- Ground.cpp
+++ Ground.cpp
@@ -1,6 +1,10 @@
 #include "Ground.h"
 #include "Engine\\Model.h"
 
+namespace
+{
+	int model_t = -1;
+}
 Ground::Ground(GameObject* parent)
 	:GameObject(parent),ground(-1)
 {
@@ -9,6 +13,8 @@ Ground::Ground(GameObject* parent)
 void Ground::Initialize()
 {
 	ground = Model::Load("masu.fbx");//1025だとうまく読み込めない可能性あり
+
+	model_t = Model::Load("Block.fbx");
 
 }
 
@@ -20,6 +26,9 @@ void Ground::Draw()
 {
 	Model::SetTransform(ground, transform_);
 	Model::Draw(ground);
+
+	Model::SetTransform(model_t, transform_);
+	Model::Draw(model_t);
 }
 
 void Ground::Release()
