da98eaf0883dbfc3d988a93b7c48dd5cd6790f2a
ShojiMakotoX <250039@jc-21.jp>
2026-07-10 15:00:21 +0900

    マップをいじる

--- Ground.cpp
+++ Ground.cpp
@@ -1,9 +1,25 @@
 #include "Ground.h"
 #include "Engine\\Model.h"
-
+#include <vector>
 namespace
 {
+	using std::vector;
 	int model_t = -1;
+
+	vector<vector<int>>mapData
+	{
+		{1,1,1,1,1,1,1,1,1,1},
+		{1,0,0,1,0,0,0,0,1,1},
+		{1,1,0,1,0,1,1,0,0,1},
+		{1,0,0,0,0,1,0,0,1,1},
+		{1,0,1,1,0,0,0,1,1,1},
+		{1,0,1,1,0,0,1,0,0,1},
+		{1,0,1,0,1,0,1,1,0,1},
+		{1,0,0,0,1,0,0,0,0,1},
+		{1,1,1,0,1,1,1,0,1,1},
+		{1,1,0,0,1,1,1,1,1,1},
+	};
+
 }
 Ground::Ground(GameObject* parent)
 	:GameObject(parent),ground(-1)
@@ -27,10 +43,20 @@ void Ground::Draw()
 	Model::SetTransform(ground, transform_);
 	Model::Draw(ground);
 
-	Transform tr;
-	tr.position_ = { 1.0f,0.0f,1.0f };
-	Model::SetTransform(model_t, transform_);
-	Model::Draw(model_t);
+	for (int j = 0;j < 10;j++)
+	{
+		for (int i = 0;i < 10;i++)
+		{
+			if (mapData[j][i] == 1)
+			{
+				Transform tr;
+				tr.position_ = { -9.0f + i * 2.0f ,0.0f,9.0f - j * 2.0f };
+				Model::SetTransform(model_t, tr);
+				Model::Draw(model_t);
+			}
+			
+		}
+	}
 }
 
 void Ground::Release()
