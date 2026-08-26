aec7b47a30e000e937563c79f2dc0a25dbfef831
ShojiMakotoX <250039@jc-21.jp>
2026-08-26 12:15:16 +0900

    敵を追加した（当たり判定を現在調整中）

--- Enemy.cpp
+++ Enemy.cpp
@@ -3,7 +3,23 @@
 #include "Player.h"
 #include "Engine\\Model.h"
 
+namespace
+{
+	std::vector<std::vector<int>>gmap;
+	enum ENEMY_DIRECTION//敵の方向変換に必要そう
+	{
+		ENEMY_UP,
+		ENEMY_DOWN,
+		ENEMY_LEFT,
+		ENEMY_RIGHT,
+		ENEMY_DIRECTION_MAX//方向の数
+	};
+	ENEMY_DIRECTION edir = ENEMY_LEFT;//最初に敵が向く方向を決定する
+	XMVECTOR E_MOVE[4] = { XMVectorSet(0,0,1,0),XMVectorSet(0,0,-1,0),
+		XMVectorSet(-1,0,0,0),XMVectorSet(1,0,0,0) };
+}
 Enemy::Enemy(GameObject* parent)
+	:GameObject(parent,"Enemy"),hModel_(-1)
 {
 }
 
@@ -11,12 +27,34 @@ void Enemy::Initialize()
 {
 	hModel_ = Model::Load("Enemy.fbx");
 	Model::SetAnimFrame(hModel_, 0, 67, 1.0);
-	transform_.position_ = { 3.0f,1.0f,5.0f };
+	transform_.position_ = { 0.0f,1.0f,0.0f };
+	transform_.scale_ = { 0.5f,0.5f,0.5f };
 }
 
 void Enemy::Update()
 {
-	
+	XMVECTOR pos = XMLoadFloat3(&transform_.position_);
+	XMVECTOR move = XMVectorSet(0, 0, 0, 0);
+	const float SPEED = 0.1f;
+	/*float angle = 0.0f;
+	static float turnFrame = 0.0f;*/
+
+	move = E_MOVE[edir];
+
+	pos = pos + SPEED * move;
+	XMStoreFloat3(&transform_.position_, pos);
+	XMFLOAT3 wpos = transform_.position_;
+
+	gmap = ground_->GetMapData();//マップを取得
+	int mapX = (int)((wpos.x) + 10.0f)/2;
+	int mapZ = (int)((10.0f - (wpos.z)))/2;
+
+	if (gmap[mapZ][mapX] == 1)
+	{
+		pos = pos - SPEED * move;
+		XMStoreFloat3(&transform_.position_, pos);
+		edir = (ENEMY_DIRECTION)(rand() % ENEMY_DIRECTION_MAX);
+	}
 }
 
 void Enemy::Draw()
